1e6c9c0a09c5de34f08d58bf9d03b2318d7672c1
Yujia Zhai <zhai.yujia627@gmail.com>
2017-06-26 20:49:35 +0800

    add two Planet constructors

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,2 +1,32 @@
 public class Planet {
+	double xxPos;
+	double yyPos;
+	double xxVel;
+	double yyVel;
+	double mass;
+	String imgFileName;
+
+   
+   /** The First Planet Constructor. */
+	public Planet (double xP, double yP, double xV,
+			       double yV, double m, String img) {
+		this.xxPos = xP;
+		this.yyPos = yP;
+		this.xxVel = xV;
+		this.yyVel = yV;
+		this.mass = m;
+		this.imgFileName = img;
+	}
+
+    
+    /** The Second Planet Constructor. */
+	public Planet(Planet p) {
+		this.xxPos = p.xxPos;
+	    this.yyPos = p.yyPos;
+	    this.xxVel = p.xxVel;
+	    this.yyVel = p.yyVel;
+	    this.mass = p.mass;
+	    this.imgFileName = p.imgFileName;
+	}
+
 }
