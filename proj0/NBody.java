3f47e77e27af7e4ad4ef81a4de1330bb9ead05f7
Yujia Zhai <zhai.yujia627@gmail.com>
2017-06-29 10:25:51 +0800

    add method readPlanet()

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,9 +1,8 @@
 public class NBody {
-/** Read radius file from data folder.
-  * @author Yujia Zhai 
-  */
 
-
+	/** Read radius file from data folder.
+ 	  * @author Yujia Zhai 
+  	  */
 	public static double readRadius(String planetsTxtPath) {
 		
 		if (planetsTxtPath == null) {
@@ -20,23 +19,38 @@ public class NBody {
 			 * R the radius of the universe, and 
 			 * finally N rows of planets data. */
 
-			/* Each column has the x- and y-coordinates of the initial postion,  then its
-			 * x- and y-components of the initial velocity, then its mass, and
+			/* Each column has the x- and y-coordinates of the initial postion,  then
+			 * its x- and y-components of the initial velocity, then its mass, and
 			 * finally the name of an image file used to display the planets. */
 
 			int N = in.readInt();
             double radius = in.readDouble();
-            /*
-			double xxPos = in.readDouble();
+
+			System.out.println(radius + " is the radius of the universe. ");
+		    return radius;
+	} 
+
+
+	/** Given a file name, return an array of Planets corresponding to the planets in the file. */
+	public static Planet[] readPlanets(String planetsTxtPath) {
+		/* Start reading in ./data/planets.txt */
+		In in = new In(planetsTxtPath);
+		int N = in.readInt();
+        double radius = in.readDouble();
+
+        Planet[] planets = new Planet[N];
+
+        for (int i = 0; i < N; i ++) {
+			double xxPos = in.readDouble();		
 			double yyPos = in.readDouble();
 			double xxVel = in.readDouble();
 			double yyVel = in.readDouble();
 			double mass = in.readDouble();
 			String imgFileName = in.readString();
-			*/
+			planets[i] = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName); /* The first constructor in Planet.java. */
 
-			System.out.println(radius + " is the radius of the universe. ");
-		    return radius;
-	} 
-	
+		}
+		return planets;
+	}
+
 } /* End of Class. */
