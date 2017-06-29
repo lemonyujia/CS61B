422458cc7d9d7ded29495d5a1bc4ef2d3fd1fca1
Yujia Zhai <zhai.yujia627@gmail.com>
2017-06-29 22:59:28 +0800

    added main method method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,6 @@
+import java.awt.Image;
+
+
 public class NBody {
 
 	/** Read radius file from data folder.
@@ -52,5 +55,59 @@ public class NBody {
 		}
 		return planets;
 	}
+
+
+    public static void main(String[] args) {
+
+        /* Initial Drawing Setion */
+    	// Collecting all needed input.
+    	double T = Double.parseDouble(args[0]);
+    	double dt = Double.parseDouble(args[1]);
+    	String filename = args[2];
+
+    	Planet[] planets = readPlanets(filename);
+    	double radius = readRadius(filename);
+
+    	// Drawing the background.
+        StdDraw.setScale(-radius, radius);	
+        StdDraw.picture(0, 0, "./images/starfield.jpg");
+
+        // Drawing all of the planets. */
+        for(Planet p : planets) {
+        	p.draw();
+        } 
+
+
+        /* Animation Creation */
+        double time = 0;
+        // Background Audio
+        StdAudio.play("./audio/2001.mid");
+        
+        while(time != T) {
+
+        	double[] xForces = new double[planets.length];
+        	double[] yForces = new double[planets.length];
+
+        	for(int i = 0; i < planets.length; i++) {
+        		xForces[i] = planets[i].calcNetForceExertedByX(planets);
+        		yForces[i] = planets[i].calcNetForceExertedByY(planets);
+        	}
+
+            for(int i = 0; i < planets.length; i++) {
+            	planets[i].update(dt, xForces[i], yForces[i]);
+            }
+        	
+        	StdDraw.picture(0, 0, "./images/starfield.jpg");
+
+        	for(Planet p : planets) {
+        	p.draw();
+        	} 
+
+        	StdDraw.show(10);
+
+            time += dt;
+        }
+  
+    } /** End of Main. */
 
 } /* End of Class. */
